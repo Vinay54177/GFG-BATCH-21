65569b73ff1c637ae64ed4e012b0ef590bc42d80
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-21 16:41:45 +0000

    Add files via upload

---
+++ DAY-3/demo-git-merge-conflict/prog1.c
@@ -0,0 +1,20 @@
+int sum(int a, int b)
+{
+    return a+b;
+}
+int mul(int a, int b)
+{
+    return a*b;
+}
+void show()
+{
+    printf("Hello GFG");
+}
+int diff(int a, int b)
+{
+    return a-b;
+}
+int div(int a, int b)
+{
+    return a/b;
+}
---
+++ DAY-3/demo-git-stash/prog1.c
@@ -0,0 +1,19 @@
+int sum(int a, int b)
+{
+    int sum=0;
+    sum= a+b;
+    return sum;
+}
+int mul(int a, int b)
+{
+    return a*b;
+    return a+b;
+}
+int diff(int a, int b)
+{
+    return a-b;
+}
+int div(int a, int b)
+{
+   return a/b; 
+}
