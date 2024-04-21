d0efa757a82e2610d73236e818a6201d859e7244
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-21 16:40:11 +0000

    Add files via upload

---
+++ DAY-4/demo-vscode-git-int/MyCalc.java
@@ -0,0 +1,18 @@
+
+public class MyCalc {
+    int sum(int a, int b)
+    {
+        int s=0;
+        s=a+b;
+        return s;
+    }
+    int diff(int a, int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args) {
+        MyCalc calc = new MyCalc();
+        System.out.println("Sum is "+calc.sum(20, 10));
+        System.out.println("Diff is "+calc.diff(20, 10));
+    }    
+}
